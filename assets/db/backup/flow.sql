d6b4114755aaf1ecae2a657c31ccca7d2cabac44
Shubham <shirpurkar.shubham@gmail.com>
2019-09-27 16:20:40 +0530

    Updated Database
    Added the Projects Table

--- assets/db/backup/flow.sql
+++ assets/db/backup/flow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2019 at 12:44 PM
+-- Generation Time: Sep 27, 2019 at 12:49 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
