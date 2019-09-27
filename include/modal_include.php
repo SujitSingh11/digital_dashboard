a49d655cd430817e71399b9ac4a105f841a86a1c
Shubham <shirpurkar.shubham@gmail.com>
2019-09-27 10:56:08 +0530

    Added Modals for login and signup
    Added login-modal for landing page.
    
    Added signup-modal for landing page.

---
+++ include/modal_include.php
@@ -0,0 +1,89 @@
+<div class="modal fade" id="modal-login" tabindex="-1" role="dialog" aria-labelledby="modal-login" aria-hidden="true">
+    <div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-body p-0">
+				<div class="card bg-secondary shadow border-0">
+				    <div class="card-header bg-white pb-5">
+				        <div class="text-muted text-center mb-3"><small>Sign in with your email address.</small></div>
+				    </div>
+				    <div class="card-body px-lg-5 py-lg-5">
+				        <form role="form">
+				            <div class="form-group mb-3">
+				                <div class="input-group input-group-alternative">
+				                    <div class="input-group-prepend">
+				                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+				                    </div>
+				                    <input class="form-control" placeholder="Email" id="email" type="email">
+				                </div>
+				            </div>
+				            <div class="form-group">
+				                <div class="input-group input-group-alternative">
+				                    <div class="input-group-prepend">
+				                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+				                    </div>
+				                    <input class="form-control" placeholder="Password" id="password" type="password">
+				                </div>
+				            </div>
+				            <div class="text-center">
+				                <button type="button" class="btn btn-primary my-4">Sign in</button>
+				            </div>
+				        </form>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="modal fade" id="modal-signup" tabindex="-1" role="dialog" aria-labelledby="modal-signup" aria-hidden="true">
+    <div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-body p-0">
+				<div class="card bg-secondary shadow border-0">
+				    <div class="card-header bg-white pb-5">
+				        <div class="text-muted text-center mb-3"><small>Sign up</small></div>
+				    </div>
+				    <div class="card-body px-lg-5 py-lg-5">
+				        <form role="form">
+				        	<div class="form-group mb-3">
+				                <div class="input-group input-group-alternative">
+				                    <div class="input-group-prepend">
+				                        <span class="input-group-text"><i class="ni ni-single-02"></i></span>
+				                    </div>
+				                    <input class="form-control" placeholder="Name" id="name" type="text">
+				                </div>
+				            </div>
+				            <div class="form-group mb-3">
+				                <div class="input-group input-group-alternative">
+				                    <div class="input-group-prepend">
+				                        <span class="input-group-text"><i class="ni ni-tie-bow"></i></span>
+				                    </div>
+				                    <input class="form-control" placeholder="Date of Birth" id="dob" type="date">
+				                </div>
+				            </div>
+				            <div class="form-group mb-3">
+				                <div class="input-group input-group-alternative">
+				                    <div class="input-group-prepend">
+				                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+				                    </div>
+				                    <input class="form-control" placeholder="Email" type="email">
+				                </div>
+				            </div>
+				            <div class="form-group">
+				                <div class="input-group input-group-alternative">
+				                    <div class="input-group-prepend">
+				                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+				                    </div>
+				                    <input class="form-control" placeholder="Password" type="password">
+				                </div>
+				            </div>
+				            <div class="text-center">
+				                <button type="button" class="btn btn-primary my-4">Sign in</button>
+				            </div>
+				        </form>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
