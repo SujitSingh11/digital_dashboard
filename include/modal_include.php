eb99e837c5d924a6ba712ece72a0b0e3f5fb1844
Shubham <shirpurkar.shubham@gmail.com>
2019-09-27 11:12:40 +0530

    Made changes to Modal, and updated the background.
    Made changes to the modal for Signup.
    
    Added a new background image.

--- include/modal_include.php
+++ include/modal_include.php
@@ -1,34 +1,34 @@
 <div class="modal fade" id="modal-login" tabindex="-1" role="dialog" aria-labelledby="modal-login" aria-hidden="true">
-    <div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
-        <div class="modal-content">
-            <div class="modal-body p-0">
+	<div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
+		<div class="modal-content">
+			<div class="modal-body p-0">
 				<div class="card bg-secondary shadow border-0">
-				    <div class="card-header bg-white pb-2">
-				        <div class="text text-center my-3">Sign in with your email address.</div>
-				    </div>
-				    <div class="card-body px-lg-5 py-lg-3">
-				        <form role="form">
-				            <div class="form-group mb-3">
-				                <div class="input-group input-group-alternative">
-				                    <div class="input-group-prepend">
-				                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-				                    </div>
-				                    <input class="form-control" placeholder="Email" id="email" type="email">
-				                </div>
-				            </div>
-				            <div class="form-group">
-				                <div class="input-group input-group-alternative">
-				                    <div class="input-group-prepend">
-				                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-				                    </div>
-				                    <input class="form-control" placeholder="Password" id="password" type="password">
-				                </div>
-				            </div>
-				            <div class="text-center">
-				                <button type="button" class="btn btn-primary my-4">Sign in</button>
-				            </div>
-				        </form>
-				    </div>
+					<div class="card-header bg-white pb-2">
+						<div class="text text-center my-3">Sign in with your email address.</div>
+					</div>
+					<div class="card-body px-lg-5 py-lg-3">
+						<form role="form">
+							<div class="form-group mb-3">
+								<div class="input-group input-group-alternative">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="ni ni-email-83"></i></span>
+									</div>
+									<input class="form-control" placeholder="Email" id="email" type="email">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="input-group input-group-alternative">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+									</div>
+									<input class="form-control" placeholder="Password" id="password" type="password">
+								</div>
+							</div>
+							<div class="text-center">
+								<button type="button" class="btn btn-primary my-4">Sign in</button>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -36,52 +36,62 @@
 </div>
 
 <div class="modal fade" id="modal-signup" tabindex="-1" role="dialog" aria-labelledby="modal-signup" aria-hidden="true">
-    <div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
-        <div class="modal-content">
-            <div class="modal-body p-0">
+	<div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
+		<div class="modal-content">
+			<div class="modal-body p-0">
 				<div class="card bg-secondary shadow border-0">
-				    <div class="card-header bg-white pb-2">
-				        <div class="text text-center my-3">Sign up</div>
-				    </div>
-				    <div class="card-body px-lg-5 py-lg-3">
-				        <form role="form">
-				        	<div class="form-group mb-3">
-				                <div class="input-group input-group-alternative">
-				                    <div class="input-group-prepend">
-				                        <span class="input-group-text"><i class="ni ni-single-02"></i></span>
-				                    </div>
-				                    <input class="form-control" placeholder="Name" id="name" type="text">
-				                </div>
-				            </div>
-				            <div class="form-group mb-3">
-				                <div class="input-group input-group-alternative">
-				                    <div class="input-group-prepend">
-				                        <span class="input-group-text"><i class="ni ni-tie-bow"></i></span>
-				                    </div>
-				                    <input class="form-control" placeholder="Date of Birth" id="dob" type="date">
-				                </div>
-				            </div>
-				            <div class="form-group mb-3">
-				                <div class="input-group input-group-alternative">
-				                    <div class="input-group-prepend">
-				                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-				                    </div>
-				                    <input class="form-control" placeholder="Email" type="email">
-				                </div>
-				            </div>
-				            <div class="form-group">
-				                <div class="input-group input-group-alternative">
-				                    <div class="input-group-prepend">
-				                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-				                    </div>
-				                    <input class="form-control" placeholder="Password" type="password">
-				                </div>
-				            </div>
-				            <div class="text-center">
-				                <button type="button" class="btn btn-primary my-4">Sign in</button>
-				            </div>
-				        </form>
-				    </div>
+					<div class="card-header bg-white pb-2">
+						<div class="text text-center my-3">Sign up</div>
+					</div>
+					<div class="card-body px-lg-5 py-lg-3">
+						<form role="form">
+							<div class="form-group mb-3">
+								<div class="input-group input-group-alternative">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="ni ni-single-02"></i></span>
+									</div>
+									<input class="form-control" placeholder="First Name" id="fname" type="text">
+					 
+									<input class="form-control" placeholder="Last Name" id="lname" type="text">
+								</div>
+							</div>
+							<div class="form-group mb-3">
+								<div class="input-group input-group-alternative">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="ni ni-tie-bow"></i></span>
+									</div>
+									<input class="form-control" placeholder="Date of Birth" id="dob" type="date">
+								</div>
+							</div>
+							<div class="form-group mb-3">
+								<div class="input-group input-group-alternative">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="ni ni-email-83"></i></span>
+									</div>
+									<input class="form-control" placeholder="Email" type="email">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="input-group input-group-alternative">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+									</div>
+									<input class="form-control" placeholder="Password" type="password">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="input-group input-group-alternative">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+									</div>
+									<input class="form-control" placeholder="Re-enter password" id="passcheck" type="password">
+								</div>
+							</div>
+							<div class="text-center">
+								<button type="button" class="btn btn-primary my-4">Sign Up</button>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
--- index.php
+++ index.php
@@ -1,80 +1,80 @@
 <?php
-    include 'assets/db/connect_db.php';
+	include 'assets/db/connect_db.php';
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Digital Dashboard</title>
-        <!--CSS Include-->
-        <?php
-            include 'include/css_include.php';
-        ?>
-        <style media="screen">
-            body {
-                background-image: url("assets/img/.jpg");
-                height: 100;
-                background-position: center;
-                background-repeat: no-repeat;
-                background-size: cover;
-              }
-            .content{
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Digital Dashboard</title>
+		<!--CSS Include-->
+		<?php
+			include 'include/css_include.php';
+		?>
+		<style media="screen">
+			body {
+				background-image: url("assets/img/bg2.jpg");
+				height: 100;
+				background-position: center;
+				background-repeat: no-repeat;
+				background-size: cover;
+			  }
+			.content{
 
-            }
-        </style>
-    </head>
-    <body>
-        <!--Navbar-->
-        <nav class="navbar navbar-horizontal navbar-expand-lg navbar-dark bg-default">
-            <div class="container">
-                <a class="navbar-brand" href="#">FLOW</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbar-default">
-                    <div class="navbar-collapse-header">
-                        <div class="row">
-                            <div class="col-6 collapse-brand">
-                                <a href="index.html">
-                                    <img src="assets/img/brand/blue.png">
-                                </a>
-                            </div>
-                            <div class="col-6 collapse-close">
-                                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span></span>
-                                    <span></span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="navbar-nav ml-lg-auto">
-                        <li class="nav-item">
-                            <button class="btn btn-default nav-link" data-toggle="modal" data-target="#modal-signup">
-                                Discover
-                            </button>
-                        </li>
-                        <li class="nav-item">
-                            <button class="btn btn-default nav-link" data-toggle="modal" data-target="#modal-login">
-                                Login
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!--Main-->
-        <div class="container content">
+			}
+		</style>
+	</head>
+	<body>
+		<!--Navbar-->
+		<nav class="navbar navbar-horizontal navbar-expand-lg navbar-dark bg-default">
+			<div class="container">
+				<a class="navbar-brand" href="#">FLOW</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbar-default">
+					<div class="navbar-collapse-header">
+						<div class="row">
+							<div class="col-6 collapse-brand">
+								<a href="index.html">
+									<img src="assets/img/brand/blue.png">
+								</a>
+							</div>
+							<div class="col-6 collapse-close">
+								<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
+									<span></span>
+									<span></span>
+								</button>
+							</div>
+						</div>
+					</div>
+					<ul class="navbar-nav ml-lg-auto">
+						<li class="nav-item">
+							<button class="btn btn-default nav-link" data-toggle="modal" data-target="#modal-signup">
+								Discover
+							</button>
+						</li>
+						<li class="nav-item">
+							<button class="btn btn-default nav-link" data-toggle="modal" data-target="#modal-login">
+								Login
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<!--Main-->
+		<div class="container content">
 
-        </div>
-        <!--Footer-->
-        <?php
-            include 'include/modal_include.php';
-        ?>
+		</div>
+		<!--Footer-->
+		<?php
+			include 'include/modal_include.php';
+		?>
 
-        <!--JS Include-->
-        <?php
-            include 'include/js_include.php';
-        ?>
-    </body>
+		<!--JS Include-->
+		<?php
+			include 'include/js_include.php';
+		?>
+	</body>
 </html>
