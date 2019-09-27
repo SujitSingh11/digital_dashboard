f0c27450809af20e2d260cd06d27937e6c498a80
Shubham <shirpurkar.shubham@gmail.com>
2019-09-27 12:57:44 +0530

    Updated login script.
    Updated login script with new warning elements, and adjusted it for the changes in database.

--- login-system/login_script.php
+++ login-system/login_script.php
@@ -1,64 +1,69 @@
 <?php
-/* User login process, checks if user exists and password is correct */
+
 require '../db/database.php';
 session_start();
 
-// Escape email to protect against SQL injections
 $email = mysqli_real_escape_string($conn,$_POST['email']);
-$result = $conn->query("SELECT * FROM users WHERE email='$email'");
+$result = $conn->query("SELECT * FROM fl_user WHERE email='$email'");
 
 if ( $result->num_rows == 0 ){
-     // User doesn't exist
-    $_SESSION['message'] = "User with that email doesn't exist!";
-    header("location: ../error.php");
+			$_SESSION['mess_type'] = 'danger';
+			$_SESSION['mess_title'] = 'Error';
+			$_SESSION['message'] = 'User with that email does not exist!';
+			header("location: ../index.php");
 }
 else {
-     // User exists
-    $user = $result->fetch_assoc();
+	 
+	$user = $result->fetch_assoc();
+	
+	if ( password_verify($_POST['password'], $user['password'])) {
+		if ($user['active'] == 1) {
+			$_SESSION['user_id'] = $user['user_id'];
+			$_SESSION['email'] = $user['email'];
+			$_SESSION['first_name'] = $user['first_name'];
+			$_SESSION['dob'] = $user['dob'];
+			$_SESSION['last_name'] = $user['last_name'];
+			$_SESSION['active'] = $user['active'];
+			$_SESSION['user_type'] = $user['user_type'];
 
-    // Password verify
-    if ( password_verify($_POST['password'], $user['password'])) {
-        // Check wether the account has been verified or not
-        if ($user['active'] == 1) {
-            // Initilize the session variables
-            $_SESSION['user_id'] = $user['user_id'];
-            $_SESSION['email'] = $user['email'];
-            $_SESSION['first_name'] = $user['first_name'];
-            $_SESSION['last_name'] = $user['last_name'];
-            $_SESSION['active'] = $user['active'];
-            $_SESSION['user_type'] = $user['user_type'];
+			$_SESSION['logged_in'] = true;
 
-            // This is how we'll know the user is logged in
-            $_SESSION['logged_in'] = true;
+			if($user['login_counter' == 0])
+			{
+				$loginplus= $conn->query("UPDATE 'fl_user' SET 'login_counter' = '1'");
+			}
 
-            // Redirect to logged-in page
-            if ($_SESSION['user_type'] == 0) {
-                $result_usertype = $conn->query("SELECT admin_id FROM admin WHERE user_id='{$_SESSION['user_id']}'");
-                $usertype = $result_usertype->fetch_assoc();
-                $_SESSION['admin_id'] = $usertype['admin_id'];
-                header("location: ../admin/admin_index.php");
-            }
-            elseif ($_SESSION['user_type'] == 1) {
-                $result_usertype = $conn->query("SELECT staff_id FROM staff WHERE user_id='{$_SESSION['user_id']}'");
-                $usertype = $result_usertype->fetch_assoc();
-                $_SESSION['staff_id'] = $usertype['staff_id'];
-                header("location: ../staff/staff_index.php");
-            }
-            elseif ($_SESSION['user_type'] == 2) {
-                $result_usertype = $conn->query("SELECT stud_id FROM student WHERE user_id='{$_SESSION['user_id']}'");
-                $usertype = $result_usertype->fetch_assoc();
-                $_SESSION['stud_id'] = $usertype['stud_id'];
-                header("location: ../student/student_index.php");
-            }
-        }
-        else{
-            $_SESSION['message'] = "Your account has not been verified please check your mail and activate your account";
-            header("location: ../index.php");
-        }
+			if ($_SESSION['user_type'] == 0) {
+				$result_usertype = $conn->query("SELECT admin_id FROM fl_admin WHERE user_id='{$_SESSION['user_id']}'");
+				$usertype = $result_usertype->fetch_assoc();
+				$_SESSION['admin_id'] = $usertype['admin_id'];
+				header("location: ../home.php");
+			}
+			elseif ($_SESSION['user_type'] == 1) {
+				$result_usertype = $conn->query("SELECT employee_id FROM fl_employee WHERE user_id='{$_SESSION['user_id']}'");
+				$usertype = $result_usertype->fetch_assoc();
+				$_SESSION['employee_id'] = $usertype['employee_id'];
+				header("location: ../home.php");
+			}
+			elseif ($_SESSION['user_type'] == 2) {
+				$result_usertype = $conn->query("SELECT manager_id FROM fl_manager WHERE user_id='{$_SESSION['user_id']}'");
+				$usertype = $result_usertype->fetch_assoc();
+				$_SESSION['manager_id'] = $usertype['manager_id'];
+				header("location: ../home.php");
+			}
+		}
+		else{
+			$_SESSION['mess_type'] = 'danger';
+			$_SESSION['mess_title'] = 'Error';
+			$_SESSION['message'] = 'User with that email does not exist!';
+			header("location: ../index.php");
+		}
 
-    }
-    else {
-        $_SESSION['message'] = "You have entered wrong password, try again!";
-        header("location: ../index.php");
-    }
+	}
+	else {
+		$_SESSION['mess_type'] = 'danger';
+			$_SESSION['mess_title'] = 'Error';
+			$_SESSION['message'] = 'You have entered wrong password, try again!';
+			header("location: ../index.php");
+	}
 }
