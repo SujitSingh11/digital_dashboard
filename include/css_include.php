7b1ae5cd32488911792d4cc149f053b6c2e3a680
baby-developer <33032751+baby-developer@users.noreply.github.com>
2019-09-27 11:18:34 +0530

    Link Added

--- include/css_include.php
+++ include/css_include.php
@@ -7,3 +7,13 @@
 <link href="assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
 <!--  CSS -->
 <link type="text/css" href="assets/css/argon-dashboard.css" rel="stylesheet">
+
+<!-- Favicon -->
+<link href="" rel="icon" type="image/png">
+<!-- Fonts -->
+<link href="" rel="stylesheet">
+<!-- Icons -->
+<link href="../assets/js/plugins/nucleo/css/nucleo.css" rel="stylesheet">
+<link href="../assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+<!--  CSS -->
+<link type="text/css" href="../assets/css/argon-dashboard.css" rel="stylesheet">
--- include/js_include.php
+++ include/js_include.php
@@ -4,3 +4,10 @@
 
 <!--  JS -->
 <script src="assets/js/argon-dashboard.min.js"></script>
+
+<!-- Core -->
+<script src="../assets/js/plugins/jquery/dist/jquery.min.js"></script>
+<script src="../assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<!--  JS -->
+<script src="../assets/js/argon-dashboard.min.js"></script>
