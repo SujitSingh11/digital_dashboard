20b29dd814cebe9d823654b4b43f038061f42509
Shubham <shirpurkar.shubham@gmail.com>
2019-09-28 09:21:50 +0530

    Made changes
    Made Changes

--- task-system/create_task.php
+++ task-system/create_task.php
@@ -5,23 +5,23 @@ session_start();
 
 $task_name = mysqli_real_escape_string($conn,$_POST['task_name']);
 $task_desc = mysqli_real_escape_string($conn,$_POST['task_desc']);
-$dept_code = mysqli_real_escape_string($conn,$_POST['dept_code']);
 $deadline = mysqli_real_escape_string($conn,$_POST['deadline']);
 $project_id = mysqli_real_escape_string($conn,$_POST['project_id']);
+$manager_id = $_SESSION['manager_id'];
 
 $slc_prj = $conn->query("SELECT * FROM fl_task WHERE project_id = '$project_id' AND task_name = '$task_name' ");
 
-if($slc_prj->num_rows > 0 ) {
+if($slc_prj->num_rows() > 0 ) {
 	$_SESSION['mess_type'] = 'danger';
 	$_SESSION['mess_title'] = 'Error';
 	$_SESSION['message'] = 'This task already exists for this project!';
-	header("location: ../project.php");)
+	header("location: ../project.php");
 }
 else
 {
-	$crt_prj = $conn->query("INSERT INTO fl_project (manager_id, project_name, project_desc, dept_code, deadline) VALUES ($_SESSION['manager_id'], '$project_name', '$project_desc', 'dept_code', '$deadline')");
+	$crt_prj = $conn->query("INSERT INTO fl_task (project_id, task_name, task_desc, manager_id, deadline) VALUES ('$project_id', '$task_name', '$task_desc', '$manager_id', '$deadline')");
 
-	if(crt_prj)
+	if(mysqli_num_rows($crt_prj) > 0)
 	{
 		$_SESSION['mess_type'] = 'success';
 		$_SESSION['mess_title'] = 'Project Created';
@@ -35,8 +35,6 @@ else
 		$_SESSION['message'] = 'Project could not be created! Contact SysAdmin.';
 		header("location: ../projects.php");
 	}
-
-
 }
 
 ?>
